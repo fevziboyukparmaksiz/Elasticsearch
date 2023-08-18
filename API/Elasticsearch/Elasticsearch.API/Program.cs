01f4880f52ea2d3d86fb13492ce447fb3d9a44c7
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-08-18 19:40:12 +0300

    ElasticClient sınıfının oluşturulması

--- API/Elasticsearch/Elasticsearch.API/Program.cs
+++ API/Elasticsearch/Elasticsearch.API/Program.cs
@@ -1,3 +1,7 @@
+using Elasticsearch.API.Extensions;
+using Elasticsearch.Net;
+using Nest;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddElastic(builder.Configuration);
 
 var app = builder.Build();
 
